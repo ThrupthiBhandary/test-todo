7c536dbf69867e46062673d077778c624d982136
ThrupthiBhandary <thrupthivh@gmail.com>
2026-07-08 20:19:02 +0530

    update: AI: Update counter.js

--- counter.js
+++ counter.js
@@ -10,6 +10,10 @@ class Counter {
   decrement(amount = 1) {
     this.value -= amount;
   }
+
+  reset() {
+    this.value = 0;
+  }
 }
 
 const counter = new Counter();
@@ -17,4 +21,7 @@ counter.increment(); // Increment by default
 console.log(counter.value); // Output: 1
 
 counter.decrement(); // Decrement by default
+console.log(counter.value); // Output: 0
+
+counter.reset(); // Reset to 0
 console.log(counter.value); // Output: 0
